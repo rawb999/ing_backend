9a2b2d547210ba8406362a1a7f057d0f1c4a7225
rawb999 <146077298+rawb999@users.noreply.github.com>
2024-03-29 23:55:04 -0700

    dfgsdfg

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def upload_image():
         return jsonify({"error": "No file submitted"}), 400
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    #app.run(debug=True, host='0.0.0.0', port=5000)
